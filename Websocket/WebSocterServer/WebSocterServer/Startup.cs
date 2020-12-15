9681256e830548f43e65b8b59b485dbc405a34d8
Chris McClure <CMcClure@penlink.com>
2020-12-15 05:23:47 -0600

    added more transactions

--- Websocket/WebSocterServer/WebSocterServer/Startup.cs
+++ Websocket/WebSocterServer/WebSocterServer/Startup.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing.Matching;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebSocketServer
@@ -22,10 +25,25 @@ namespace WebSocketServer
 
             app.Use(async (context, next) =>
             {
+                WriteRequestParam(context);
                 if (context.WebSockets.IsWebSocketRequest)
                 {
                     WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
                     Console.WriteLine($"Websocket Connected");
+
+                    await ReceivedMessage(webSocket, (result, buffer) =>
+                    {
+                        if (result.MessageType == WebSocketMessageType.Text)
+                        {
+                            Console.WriteLine("Message Received");
+                            return;
+                        }
+                        else if (result.MessageType == WebSocketMessageType.Close)
+                        {
+                            Console.WriteLine("Received Close Message");
+                            return;
+                        }
+                    });
                 }
                 else
                 {
@@ -40,6 +58,31 @@ namespace WebSocketServer
                 Console.WriteLine("Hello from the third request delegate");
                 await context.Response.WriteAsync("Hello from the third request delegate");
             });
+        }
+
+        public void WriteRequestParam(HttpContext context)
+        {
+            Console.WriteLine("request method " + context.Request.Method);
+            Console.WriteLine("request protocol " + context.Request.Protocol);
+
+            if (context.Request.Headers != null)
+            {
+                foreach (var header in context.Request.Headers)
+                {
+                    Console.WriteLine($"--> {header.Key}  value: {header.Value}");
+                    
+                }
+            }
+        }
+
+        private async Task ReceivedMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer), CancellationToken.None);
+                handleMessage(result, buffer);
+            }
         }
     }
 }
