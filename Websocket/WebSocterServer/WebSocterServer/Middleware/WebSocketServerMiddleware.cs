82689b8e3315a3ae8e64ca995c107a0eb19d1c84
Chris McClure <CMcClure@penlink.com>
2020-12-16 21:03:31 -0600

    Added middleware manager

--- Websocket/WebSocterServer/WebSocterServer/Middleware/WebSocketServerMiddleware.cs
+++ Websocket/WebSocterServer/WebSocterServer/Middleware/WebSocketServerMiddleware.cs
@@ -3,6 +3,7 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Http;
 
 namespace WebSocketServer.Middleware
@@ -11,18 +12,24 @@ namespace WebSocketServer.Middleware
     {
         private readonly RequestDelegate _Next;
 
-        public WebSocketServerMiddleware(RequestDelegate next)
+        private readonly WebsocketServerConnectionManager _Manager;
+
+        public WebSocketServerMiddleware(RequestDelegate next, WebsocketServerConnectionManager manager)
         {
+            _Manager = manager;
             _Next = next;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-          
             if (context.WebSockets.IsWebSocketRequest)
             {
                 WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
                 Console.WriteLine($"Websocket Connected");
+
+                string connId = _Manager.AddSocket(webSocket);
+
+                await SendConnIdAsync(webSocket, connId);
 
                 await ReceivedMessage(webSocket, (result, buffer) =>
                 {
@@ -61,7 +68,7 @@ namespace WebSocketServer.Middleware
             }
         }
 
-        private async Task ReceivedMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        private static async Task ReceivedMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
         {
             var buffer = new byte[1024 * 4];
             while (socket.State == WebSocketState.Open)
@@ -71,6 +78,11 @@ namespace WebSocketServer.Middleware
             }
         }
 
-      
+        private async Task SendConnIdAsync(WebSocket socket, string connId)
+        {
+            var buffer = Encoding.UTF8.GetBytes($"ConnID: {connId}");
+
+            await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
     }
 }
---
+++ Websocket/WebSocterServer/WebSocterServer/Middleware/WebsocketServerConnectionManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Net.WebSockets;
+
+namespace WebSocketServer.Middleware
+{
+    public class WebsocketServerConnectionManager
+    {
+
+        private readonly ConcurrentDictionary<string, WebSocket> _Sockets = new ConcurrentDictionary<string, WebSocket>();
+
+        public ConcurrentDictionary<string, WebSocket> GetAllSockets()
+        {
+            return _Sockets;
+        }
+
+        public string AddSocket(WebSocket socket)
+        {
+            string connId = Guid.NewGuid().ToString();
+
+            _Sockets.TryAdd(connId, socket);
+
+            Console.WriteLine($"Connection {connId} added");
+            
+            return connId;
+
+        }
+
+    }
+}
