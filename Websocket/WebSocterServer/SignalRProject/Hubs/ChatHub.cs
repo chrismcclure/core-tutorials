72600583d56021de6ec4237cdac3d8ee65358a89
Chris McClure <CMcClure@penlink.com>
2020-12-19 17:17:52 -0600

    SignalR client and getting bits installed

--- Websocket/WebSocterServer/SignalRProject/Hubs/ChatHub.cs
+++ Websocket/WebSocterServer/SignalRProject/Hubs/ChatHub.cs
@@ -1,10 +1,37 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
 
 namespace SignalRProject.Hubs
 {
     public class ChatHub : Hub
     {
+        public override Task OnConnectedAsync()
+        {
 
+            Console.WriteLine($"--> connected.  {Context.ConnectionId}");
 
+            Clients.Client(Context.ConnectionId).SendAsync("ReceiveConnID", Context.ConnectionId);
+
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            var routeOb = JsonConvert.DeserializeObject<dynamic>(message);
+            string toClient = routeOb.To;
+
+            if (string.IsNullOrEmpty(toClient))
+            {
+                await Clients.All.SendAsync("ReceivedMessage", message);
+            }
+            else
+            {
+                await Clients.Client(toClient).SendAsync($"received message", message);
+            }
+
+            Console.WriteLine($"Message Received on {Context.ConnectionId}");
+        }
     }
 }
--- Websocket/WebSocterServer/SignalRProject/Startup.cs
+++ Websocket/WebSocterServer/SignalRProject/Startup.cs
@@ -1,39 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SignalRProject.Hubs;
 
 namespace SignalRProject
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddSignalR();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [System.Obsolete]
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            //this is for local host. 
+            app.UseCors(builder =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                //Don't do this in production, just some examples.
+                builder.WithOrigins("null")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
 
-            app.UseRouting();
+            });
 
-            app.UseEndpoints(endpoints =>
+            app.UseSignalR(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapHub<ChatHub>("/chatHub");
             });
         }
     }
